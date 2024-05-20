896a11204a05cf4ebcd9817f3d1dc55eddc81e60
rlouvrie <rlouvrie@student.42.fr>
2024-05-20 14:38:23 +0200

    adding documentation for urls.py

--- lettings/urls.py
+++ lettings/urls.py
@@ -1,3 +1,12 @@
+"""
+This module defines the URL configurations for the 'lettings' Django application.
+
+It imports the necessary modules and defines a 'urlpatterns' list which routes URLs to views.
+
+Each URL is mapped to a view function in the 'views' module. When Django encounters an incoming URL, it checks the 'urlpatterns' list from top to bottom until it finds a matching pattern.
+"""
+
+
 from django.urls import path
 from .views import index, letting
 
--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,3 +1,11 @@
+"""
+This module defines the URL configurations for the 'oc_lettings_site' Django application.
+
+It imports the necessary modules and defines a 'urlpatterns' list which routes URLs to views.
+
+Each URL is mapped to a view function in the 'views' module. When Django encounters an incoming URL, it checks the 'urlpatterns' list from top to bottom until it finds a matching pattern.
+"""
+
 from django.contrib import admin
 from django.urls import path, include
 
