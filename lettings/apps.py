a4a00bd024c302aa9ab7cfafc9b2b2f099c742be
rlouvrie <rlouvrie@student.42.fr>
2024-05-20 13:52:26 +0200

    Documenting apps.py files ok

--- lettings/apps.py
+++ lettings/apps.py
@@ -1,5 +1,21 @@
+"""
+This module contains the Django application configuration for the project.
+
+It defines an AppConfig class that contains the application's configuration settings.
+This class is used by Django to initialize the application and its components.
+
+Each Django application should have an apps.py module with a derived AppConfig class.
+"""
+
 from django.apps import AppConfig
 
 
 class LettingsConfig(AppConfig):
+    """
+    This class contains the configuration for the 'lettings' application.
+
+    It inherits from Django's AppConfig class and overrides the 'name' attribute with the name of the application.
+
+    Django uses this configuration when initializing the 'lettings' application.
+    """
     name = "lettings"
