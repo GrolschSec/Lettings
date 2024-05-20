2f9fc44e20515923435bcda93596cdd7d1116317
rlouvrie <rlouvrie@student.42.fr>
2024-05-20 14:32:07 +0200

    adding asgi module documentation

--- oc_lettings_site/asgi.py
+++ oc_lettings_site/asgi.py
@@ -1,5 +1,13 @@
-import os
+"""
+This module configures the ASGI application for Django.
 
+ASGI (Asynchronous Server Gateway Interface) is the standard for Python asynchronous web apps and servers.
+This module sets the DJANGO_SETTINGS_MODULE environment variable to point to the Django project's settings and then gets the ASGI application instance.
+
+The application instance is used by the ASGI server to interact with the Django project.
+"""
+
+import os
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oc_lettings_site.settings")
