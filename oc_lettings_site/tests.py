7622ca2413b3b603c1bb5414b5886f7a1bfcab99
rlouvrie <rlouvrie@student.42.fr>
2024-05-20 14:34:35 +0200

    adding generical docstring for tests.py modules and settings.py module

--- oc_lettings_site/tests.py
+++ oc_lettings_site/tests.py
@@ -1,2 +1,11 @@
+"""
+This module contains the unit tests for the Django application.
+
+Each function in this module is a test case, and should start with the word 'test_'.
+The tests can be run using the 'python manage.py test' command.
+
+These tests help ensure the correct functionality of the application by testing its individual units of code.
+"""
+
 def test_dummy():
     assert 1
--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,12 @@
+"""
+This module contains the unit tests for the Django application.
+
+Each function in this module is a test case, and should start with the word 'test_'.
+The tests can be run using the 'python manage.py test' command.
+
+These tests help ensure the correct functionality of the application by testing its individual units of code.
+"""
+
 # from django.test import TestCase
 
 # Create your tests here.
