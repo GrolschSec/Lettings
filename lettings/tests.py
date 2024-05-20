141728e236380ed0b8fa637d5628da24901155d2
rlouvrie <rlouvrie@student.42.fr>
2024-05-20 17:09:13 +0200

    first test added for lettings models

--- lettings/tests.py
+++ lettings/tests.py
@@ -8,6 +8,49 @@ These tests help ensure the correct functionality of the application by testing 
  units of code.
 """
 
-# from django.test import TestCase
+from django.test import TestCase
+from .models import Address, Letting
 
-# Create your tests here.
+class AddressModelUnitTest(TestCase):
+    def setUp(self):
+        self.address = Address.objects.create(
+            number=123,
+            street='Test Street',
+            city='Test City',
+            state='TS',
+            zip_code=12345,
+            country_iso_code='TSC'
+        )
+
+    def test_address_creation(self):
+        self.assertEqual(self.address.number, 123)
+        self.assertEqual(self.address.street, 'Test Street')
+        self.assertEqual(self.address.city, 'Test City')
+        self.assertEqual(self.address.state, 'TS')
+        self.assertEqual(self.address.zip_code, 12345)
+        self.assertEqual(self.address.country_iso_code, 'TSC')
+
+    def test_address_str(self):
+        self.assertEqual(str(self.address), '123 Test Street')
+
+class LettingModelUnitTest(TestCase):
+    def setUp(self):
+        self.address = Address.objects.create(
+            number=123,
+            street='Test Street',
+            city='Test City',
+            state='TS',
+            zip_code=12345,
+            country_iso_code='TSC'
+        )
+        self.letting = Letting.objects.create(
+            address=self.address,
+            title='Test Letting',
+        )
+
+    def test_letting_creation(self):
+        self.assertEqual(self.letting.address, self.address)
+        self.assertEqual(self.letting.title, 'Test Letting')
+    
+    def test_letting_str(self):
+        self.assertEqual(str(self.letting), 'Test Letting')
